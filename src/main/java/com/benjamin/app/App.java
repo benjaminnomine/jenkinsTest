eef30e05987ee910beec76442df9d19078377b6a
benjaminnomine <benjamin.nomine@outlook.fr>
2020-11-27 20:46:25 +0000

    initial commit

---
+++ src/main/java/com/benjamin/app/App.java
@@ -0,0 +1,13 @@
+package com.benjamin.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
